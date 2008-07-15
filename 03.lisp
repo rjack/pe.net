e0f94a32e3c731dd47a9b573ca43a3d8339e6bad
Giacomo Ritucci <ritucci@cs.unibo.it>
2008-07-15 02:04:29 +0200

    Risolto problema 3

--- 03.lisp
+++ 03.lisp
@@ -1,7 +1,14 @@
-;;;; Giacomo Ritucci, 23.07 di venerdì 14 dicembre 2007
+;;;; Giacomo Ritucci, 02.03 di martedì 15 luglio 2008
 
 ;;; Problem 3
 ;;;
 ;;; The prime factors of 13195 are 5, 7, 13 and 29.
 ;;;
 ;;; What is the largest prime factor of the number 317584931803?
+
+(defun largest-prime-factor (n &optional (i 2))
+  (cond ((= n i) n)
+	((= 0 (mod n i)) (largest-prime-factor (/ n i) i))
+	(T (largest-prime-factor n (incf i)))))
+
+(largest-prime-factor 600851475143)
