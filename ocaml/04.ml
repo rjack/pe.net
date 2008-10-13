78899340a3bb20cd8b33e305e4a67263c2a0a8bd
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-10-13 15:46:08 +0200

    [04 ocaml] Avanti a tentoni
    ocaml/04.ml:
    
    	* doc e indentazione funzioni.
    
    	* (***) nuovo operatore infisso per esponenziale su interi.
    
    	* (list_numbers, list_n_digits_numbers) nuove funzioni.

--- ocaml/04.ml
+++ ocaml/04.ml
@@ -10,24 +10,50 @@
 open String;;
 
 
+(* True if the first and the last charachers in a string are equal. *)
 let extremes_match =
-  fun str -> (str.[0] = str.[length str - 1]);;
+  fun str ->
+    (str.[0] = str.[length str - 1]);;
 
 
+(* Integer exponentiation *)
+let ( *** ) =
+  fun x y ->
+    int_of_float ((float_of_int x) ** (float_of_int y));;
+
+
+(* example: "hello" -> "ell" *)
 let strip_extremes =
-  fun str -> (sub str 1 (length str - 2));;
+  fun str ->
+    (sub str 1 (length str - 2));;
 
 
 let rec is_palindromic =
-  fun str -> match length str with
-      1 -> true
-    | 2 when (extremes_match str) -> true
-    | _ when (extremes_match str
-              && is_palindromic (strip_extremes str)) -> true
-    | _ -> false;;
+  fun str ->
+    match length str with
+        1                                               -> true
+      | 2 when (extremes_match str)                     -> true
+      | _ when (extremes_match str
+                && is_palindromic (strip_extremes str)) -> true
+      | _                                               -> false;;
 
 
 let max =
-  fun x y -> match x < y with
-      true  -> y
-    | false -> x;;
+  fun x y ->
+    match x < y with
+        true  -> y
+      | false -> x;;
+
+
+let rec list_numbers =
+  fun int_start int_end ->
+    match int_end - int_start with
+        0 -> [ int_end ]
+      | _ -> int_start :: (list_numbers (int_start + 1) int_end);;
+
+
+let list_n_digits_numbers =
+  fun n ->
+    match n with
+        0 -> []
+      | _ -> list_numbers (10 *** (n - 1)) ((10 *** n) - 1);;
